b6d9752330dc3caee969bdc1f0530d8ee0e151ae
andy5995 <arch_stanton5995@proton.me>
2025-04-26 23:20:21 -0500

    initial commit

---
+++ src/globals.h
@@ -0,0 +1,11 @@
+#include "config.h"
+
+/* can be set on the command line with -v */
+/* Show all 52 cards after the deck has been shuffled */
+extern bool verbose;
+
+/* can be set on the command line with -s */
+/* Show the hand that was dealt */
+extern bool SHOW_HAND;
+
+#define CR printf("\n")
