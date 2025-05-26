a754f180cc2a8ac7b4afffcb0cf3bf6676f650ec
andy5995 <arch_stanton5995@proton.me>
2025-05-26 03:04:30 -0500

    Add debug.{c,h}

---
+++ src/debug.c
@@ -0,0 +1,56 @@
+/*
+ debug.c
+ https://github.com/Dealer-s-Choice/dealers_choice
+
+ MIT License
+
+ Copyright (c) 2025 Andy Alt
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#include "debug.h"
+#include "game.h"
+
+DebugPrintCards_t debug_print_cards(struct pokeval_hand_t *hand) {
+  DebugPrintCards_t str = {0};
+  char *ptr = str.str;
+  for (int i = 0; i < HAND_SIZE; i++) {
+    if (is_dh_card_back(hand->card[i])) {
+      fprintf(stderr, "-BACK-");
+      continue;
+    }
+    if (is_dh_card_null(hand->card[i])) {
+      fprintf(stderr, "-BACK-");
+      continue;
+    }
+    char result[20];
+    snprintf(result, sizeof result, "%s%s", get_card_face(hand->card[i]),
+             get_card_unicode_suit(hand->card[i]));
+    fprintf(stderr, "%s", result);
+    size_t len = strlen(str.str);
+    snprintf(ptr, sizeof str.str - len, "%s", result);
+    ptr += strlen(result);
+  }
+  return str;
+}
--- tests/debug_print_cards.c
+++ tests/debug_print_cards.c
@@ -2,7 +2,7 @@
 
 _MAIN_HEAD_
 
-Game_State game_state;
+GameState_t game_state;
 init_game_state(&game_state);
 
 for (int i = 0; i < 3; i++) {
@@ -13,8 +13,8 @@ struct dh_deck deck = dh_get_new_deck();
 dh_pcg_srand(1, 1);
 dh_shuffle_deck(&deck);
 
-struct player_t *dealer = &game_state.player[0];
-RealHand real_hand =
+Player_t *dealer = &game_state.player[0];
+RealHand_t real_hand =
     deal_cards_to_players(&game_state, dealer, &deck, game_choices[FIVE_CARD_DRAW].game_type);
 DebugPrintCards_t cards_str = debug_print_cards(&real_hand.player[0]);
 
