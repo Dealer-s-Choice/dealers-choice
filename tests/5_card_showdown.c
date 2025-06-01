6e0dbe5142e80abc0cb58e8c1c402a8ec54c9d74
andy5995 <arch_stanton5995@proton.me>
2025-06-01 10:04:10 -0500

    More clean up, increase some delays (trying to fix net test on MacOS)

--- tests/5_card_showdown.c
+++ tests/5_card_showdown.c
@@ -29,72 +29,74 @@ int main(int argc, char *argv[]) {
   }
 
   for (int game = 0; game < n_passes; game++) {
-    sleep(1);
-    for (int i = 0; i < 2; i++) {
+    const int n_seconds = 1;
+    sleep(n_seconds);
+    int i;
+
+    sleep(n_seconds);
+    for (i = 0; i < 2; i++) {
       assert(recv_game_state(socket_context[i].sock, socket_context[i].set, &game_state[i],
                              &client_state[i], socket_context[i].id) != RECV_ERROR);
       assert(socket_context[i].sock != NULL);
     }
 
-    sleep(1);
-    fprintf(stderr, "Dealer %d selecting game\n", game_state[0].dealer_id);
-    assert(send_game_select(socket_context[game_state[0].dealer_id].sock,
+    const int dealer_id = game_state[0].dealer_id;
+    switch (game) {
+    case 0:
+      assert(dealer_id == 0);
+      break;
+    case 1:
+      assert(dealer_id == 1);
+      break;
+    case 2:
+      assert(dealer_id == 0);
+      break;
+    }
+
+    sleep(n_seconds);
+    fprintf(stderr, "Dealer %d selecting game\n", dealer_id);
+    assert(send_game_select(socket_context[dealer_id].sock,
                             game_choices[FIVE_CARD_SHOWDOWN].game_type) == 0);
 
-    sleep(1);
-    for (int i = 0; i < 2; i++) {
+    sleep(n_seconds);
+    for (i = 0; i < 2; i++) {
       assert(recv_game_state(socket_context[i].sock, socket_context[i].set, &game_state[i],
                              &client_state[i], socket_context[i].id) != RECV_ERROR);
       assert(socket_context[i].sock != NULL);
     }
 
-    sleep(1);
+    sleep(n_seconds);
 
     assert(send_player_action(socket_context[game_state[0].turn_id].sock, ACTION_BET, 500) == 0);
 
-    for (int i = 0; i < 2; i++) {
+    for (i = 0; i < 2; i++) {
       debug_print_cards(&game_state[i].player[i].hand);
       fputc('\n', stderr);
     }
 
-    sleep(1);
-    for (int i = 0; i < 2; i++) {
-      assert(recv_game_state(socket_context[i].sock, socket_context[i].set, &game_state[i],
-                             &client_state[i], socket_context[i].id) != RECV_ERROR);
+    for (int recv = 0; recv < 2; recv++) {
+      sleep(n_seconds);
+      for (i = 0; i < 2; i++) {
+        assert(recv_game_state(socket_context[i].sock, socket_context[i].set, &game_state[i],
+                               &client_state[i], socket_context[i].id) != RECV_ERROR);
+        assert(socket_context[i].sock != NULL);
+      }
     }
 
-    sleep(1);
-    for (int i = 0; i < 2; i++) {
-      assert(recv_game_state(socket_context[i].sock, socket_context[i].set, &game_state[i],
-                             &client_state[i], socket_context[i].id) != RECV_ERROR);
-    }
-
-    sleep(1);
+    sleep(n_seconds);
     assert(send_player_action(socket_context[game_state[0].turn_id].sock, ACTION_CALL, 0) == 0);
 
-    sleep(1);
-    for (int i = 0; i < 2; i++) {
-      assert(recv_game_state(socket_context[i].sock, socket_context[i].set, &game_state[i],
-                             &client_state[i], socket_context[i].id) != RECV_ERROR);
+    for (int recv = 0; recv < 4; recv++) {
+      sleep(n_seconds);
+      for (i = 0; i < 2; i++) {
+        assert(recv_game_state(socket_context[i].sock, socket_context[i].set, &game_state[i],
+                               &client_state[i], socket_context[i].id) != RECV_ERROR);
+        assert(socket_context[i].sock != NULL);
+      }
     }
 
-    sleep(1);
-    for (int i = 0; i < 2; i++) {
-      assert(recv_game_state(socket_context[i].sock, socket_context[i].set, &game_state[i],
-                             &client_state[i], socket_context[i].id) != RECV_ERROR);
-    }
-
-    sleep(1);
-    for (int i = 0; i < 2; i++) {
-      assert(recv_game_state(socket_context[i].sock, socket_context[i].set, &game_state[i],
-                             &client_state[i], socket_context[i].id) != RECV_ERROR);
-    }
-
-    sleep(1);
-    for (int i = 0; i < 2; i++) {
-      assert(recv_game_state(socket_context[i].sock, socket_context[i].set, &game_state[i],
-                             &client_state[i], socket_context[i].id) != RECV_ERROR);
-      assert(socket_context[i].sock != NULL);
+    sleep(n_seconds);
+    for (i = 0; i < 2; i++) {
       fprintf(stderr, "%d: %d\n", i, game_state[i].player[i].coins);
     }
     fprintf(stderr, "%d\n", game_state[0].pot);
@@ -113,7 +115,7 @@ int main(int argc, char *argv[]) {
       assert(game_state[0].player[1].coins == 19250);
       break;
     }
-    sleep(1);
+    sleep(n_seconds);
   }
 
   sleep(1);
--- tests/5_card_stud.c
+++ tests/5_card_stud.c
@@ -30,75 +30,76 @@ int main(int argc, char *argv[]) {
 
   for (int game = 0; game < n_passes; game++) {
     fprintf(stderr, "\n-#- game: %d\n", game);
-    sleep(1);
-    for (int i = 0; i < 2; i++) {
+    const int n_seconds = 1;
+    sleep(n_seconds);
+    int i;
+    for (i = 0; i < 2; i++) {
       assert(recv_game_state(socket_context[i].sock, socket_context[i].set, &game_state[i],
                              &client_state[i], socket_context[i].id) != RECV_ERROR);
       assert(socket_context[i].sock != NULL);
     }
 
-    sleep(1);
-    fprintf(stderr, "Dealer %d selecting game\n", game_state[0].dealer_id);
+    const int dealer_id = game_state[0].dealer_id;
+    switch (game) {
+    case 0:
+      assert(dealer_id == 0);
+      break;
+    case 1:
+      assert(dealer_id == 1);
+      break;
+    case 2:
+      assert(dealer_id == 0);
+      break;
+    }
+
+    sleep(n_seconds);
+    fprintf(stderr, "Dealer %d selecting game\n", dealer_id);
     assert(send_game_select(socket_context[game_state[0].dealer_id].sock,
                             game_choices[FIVE_CARD_STUD].game_type) == 0);
 
-    sleep(1);
-    for (int i = 0; i < 2; i++) {
+    sleep(n_seconds);
+    for (i = 0; i < 2; i++) {
       assert(recv_game_state(socket_context[i].sock, socket_context[i].set, &game_state[i],
                              &client_state[i], socket_context[i].id) != RECV_ERROR);
-      // assert(socket_context[i].sock != NULL);
     }
 
     for (int n_rounds = 0; n_rounds < game_choices[FIVE_CARD_STUD].n_betting_rounds; n_rounds++) {
       fprintf(stderr, "\n -#- game: %d -#- n_rounds: %d\n", game, n_rounds);
-      sleep(1);
+      sleep(n_seconds);
 
       fprintf(stderr, "turn_id: %d sending bet...\n", game_state[0].turn_id);
       assert(send_player_action(socket_context[game_state[0].turn_id].sock, ACTION_BET, 500) == 0);
 
-      for (int i = 0; i < 2; i++) {
+      for (i = 0; i < 2; i++) {
         debug_print_cards(&game_state[i].player[i].hand);
         fputc('\n', stderr);
       }
 
-      sleep(1);
-      for (int i = 0; i < 2; i++) {
-        assert(recv_game_state(socket_context[i].sock, socket_context[i].set, &game_state[i],
-                               &client_state[i], socket_context[i].id) != RECV_ERROR);
+      for (int recv = 0; recv < 3; recv++) {
+        sleep(n_seconds);
+        for (i = 0; i < 2; i++) {
+          assert(recv_game_state(socket_context[i].sock, socket_context[i].set, &game_state[i],
+                                 &client_state[i], socket_context[i].id) != RECV_ERROR);
+          assert(socket_context[i].sock != NULL);
+        }
       }
 
-      sleep(1);
-      for (int i = 0; i < 2; i++) {
-        assert(recv_game_state(socket_context[i].sock, socket_context[i].set, &game_state[i],
-                               &client_state[i], socket_context[i].id) != RECV_ERROR);
-      }
-
-      sleep(1);
+      sleep(n_seconds);
 
       fprintf(stderr, "turn_id: %d\n", game_state[0].turn_id);
       assert(send_player_action(socket_context[game_state[0].turn_id].sock, ACTION_CALL, 0) == 0);
 
-      // sleep(1);
-      for (int i = 0; i < 2; i++) {
-        assert(recv_game_state(socket_context[i].sock, socket_context[i].set, &game_state[i],
-                               &client_state[i], socket_context[i].id) != RECV_ERROR);
-      }
-
-      // sleep(1);
-      for (int i = 0; i < 2; i++) {
-        assert(recv_game_state(socket_context[i].sock, socket_context[i].set, &game_state[i],
-                               &client_state[i], socket_context[i].id) != RECV_ERROR);
-      }
-
-      // sleep(1);
-      for (int i = 0; i < 2; i++) {
-        assert(recv_game_state(socket_context[i].sock, socket_context[i].set, &game_state[i],
-                               &client_state[i], socket_context[i].id) != RECV_ERROR);
+      for (int recv = 0; recv < 3; recv++) {
+        sleep(n_seconds);
+        for (i = 0; i < 2; i++) {
+          assert(recv_game_state(socket_context[i].sock, socket_context[i].set, &game_state[i],
+                                 &client_state[i], socket_context[i].id) != RECV_ERROR);
+        }
       }
     }
 
-    sleep(1);
-    for (int i = 0; i < 2; i++) {
+    sleep(n_seconds);
+    for (i = 0; i < 2; i++) {
       assert(recv_game_state(socket_context[i].sock, socket_context[i].set, &game_state[i],
                              &client_state[i], socket_context[i].id) != RECV_ERROR);
       fprintf(stderr, "%d: %d\n", i, game_state[i].player[i].coins);
@@ -106,7 +107,7 @@ int main(int argc, char *argv[]) {
     }
     fprintf(stderr, "%d\n", game_state[0].pot);
 
-    sleep(1);
+    sleep(n_seconds);
 
     switch (game) {
     case 0:
@@ -122,7 +123,7 @@ int main(int argc, char *argv[]) {
       assert(game_state[0].player[1].coins == 22000);
       break;
     }
-    sleep(1);
+    sleep(n_seconds);
   }
 
   sleep(2);
