d61d004f29893b39580425d2518fd8448f8434b1
andy5995 <arch_stanton5995@proton.me>
2025-06-03 13:42:08 -0500

    tests: Add a couple more delays

--- tests/00_test.h
+++ tests/00_test.h
@@ -35,27 +35,27 @@
                                                                                                    \
   GameState_t game_state[2] = {0};                                                                 \
   ClientState_t client_state[2] = {0};                                                             \
-  PlayerConfig_t player_config = {0};                                                              \
+  PlayerConfig_t player_config = get_player_config();                                              \
   const bool test_mode = true;                                                                     \
   char addr[] = "127.0.0.1";                                                                       \
   SocketContext_t socket_context[2];                                                               \
+  const int n_seconds = 1;                                                                         \
                                                                                                    \
   for (int i = 0; i < 2; i++) {                                                                    \
     socket_context[i] =                                                                            \
         get_socket_context_and_run_client(&player_config, addr, &sdl_context, &font, test_mode);   \
-    sleep(1);                                                                                      \
     assert(socket_context[i].sock != NULL);                                                        \
+    sleep(n_seconds);                                                                              \
   }                                                                                                \
                                                                                                    \
   for (int game = 0; game < n_passes; game++) {                                                    \
     fprintf(stderr, "\n-#- game: %d\n", game);                                                     \
-    const int n_seconds = 1;                                                                       \
     sleep(n_seconds);                                                                              \
     int i;                                                                                         \
                                                                                                    \
     for (int recv = 0; recv < 2; recv++) {                                                         \
-      sleep(n_seconds);                                                                            \
       for (i = 0; i < 2; i++) {                                                                    \
+        sleep(n_seconds);                                                                          \
         assert(recv_game_state(socket_context[i].sock, socket_context[i].set, &game_state[i],      \
                                &client_state[i], socket_context[i].id) != RECV_ERROR);             \
         assert(socket_context[i].sock != NULL);                                                    \
