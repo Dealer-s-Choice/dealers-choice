4a2ce8aaaf16b4bbe443be501544b02569080644
andy5995 <arch_stanton5995@proton.me>
2025-06-26 02:26:24 -0500

    Add cmake (for miniaudio)

---
+++ .github/scripts/install_ubuntu_deps.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+sudo apt update && sudo apt upgrade -y
+sudo apt-get install --no-install-recommends -y \
+  cmake \
+  gettext \
+  meson \
+  libprotobuf-c-dev \
+  libsdl2-dev \
+  libsdl2-image-dev \
+  libsdl2-net-dev \
+  libsdl2-ttf-dev
