da6e6b9464a3e20a819608c06b053ac90cc7c6f9
andy5995 <arch_stanton5995@proton.me>
2026-04-14 18:18:11 -0500

    scripts/tmux-run-3-gui-clients.sh: Update [skip ci]

--- scripts/tmux-run-3-gui-clients.sh
+++ scripts/tmux-run-3-gui-clients.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 #
+# 2026
+#
 # Starts 3 clients in a tmux session
 #
 # Used for testing
 
 set -ev
 
-tmux new-session -d -s dealers "bash -c './dealers-choice --verbose; exec bash'" \; \
+tmux new-session -d -s dctest "bash -c './dealers-choice --verbose; exec bash'" \; \
   new-window "bash -c './dealers-choice --verbose; exec bash'" \; \
   new-window "bash -c './dealers-choice --verbose; exec bash'" \; \
   attach
